157c12c7b2b82a024d8163b56135cdb57ebaf244
Kamrul <19103030@iubat.edu>
2019-11-12 22:54:20 +0600

    Added Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout<<"Welcome To Sum: "<<endl;
+    int a,b,sum=0;
+    cout<<"Enter 1st integer: ";
+    cin>>a;
+    cout<<"Enter 2nd integer: ";
+    cin>>b;
+
+    sum=a+b;
+    cout<<"Sum of two integer: "<<sum<<endl;
+    
+    getch();
+}
